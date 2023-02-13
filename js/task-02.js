8eed3bfab3fcb18437692bcff444b8277297e3b8
Sergii Podobrii <podobri@ukr.net>
2023-02-13 15:00:52 +0200

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+
+const listElements = ingredients.map((element) => {
+  const listRef = document.createElement("li");
+  listRef.classList.add("item");
+  listRef.textContent = element;
+  return listRef;
+});
+
+listIngredients.append(...listElements);
+
+console.log(listElements);
