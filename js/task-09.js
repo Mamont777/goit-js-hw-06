dcc5c28d5f3d155eae69a3b96130ce601962b7a0
Sergii Podobrii <podobri@ukr.net>
2023-02-14 18:21:32 +0200

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const textColor = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+const changeColor = () => {
+  textColor.textContent = body.style.backgroundColor = getRandomHexColor();
+};
+changeColorBtn.addEventListener("click", changeColor);
