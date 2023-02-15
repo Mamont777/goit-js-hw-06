bf8f79005e89753d8463b5d90bd934657da9f03f
Sergii Podobrii <podobri@ukr.net>
2023-02-15 21:17:47 +0200

    Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  input: document.querySelector("[type='number']"),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+refs.createBtn.addEventListener("click", createBoxes);
+refs.destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  amount = refs.input.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    let box = document.createElement("div");
+
+    box.classList.add("box");
+    box.style.width = `${boxSize + i * 10}px`;
+    box.style.height = `${boxSize + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    refs.boxes.append(box);
+  }
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
